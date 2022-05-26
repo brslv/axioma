d03d5cbfaca4e93e418031946e0e61fbcba36f89
Borislav Grigorov <borislav.b.grigorov@gmail.com>
2022-05-26 19:18:47 +0300

    feat: migrate to chrome.storage API

--- src/hooks/usePersistence.tsx
+++ src/hooks/usePersistence.tsx
@@ -11,18 +11,53 @@ export default function usePersistence<T>({
 }) {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (!isLoaded) {
-      const stateFromStorage = localStorage.getItem(key);
+  const getLocalStorageState = (key: string) => {
+    const state = localStorage.getItem(key);
 
-      if (stateFromStorage)
-        onLoadedFromStorage(JSON.parse(stateFromStorage) as T);
-
-      setIsLoaded(true);
+    try {
+      return state ? JSON.parse(state) : null;
+    } catch (e: unknown) {
+      return null;
     }
+  };
+
+  const removeLocalStorageState = (key: string) => {
+    localStorage.removeItem(key);
+  };
+
+  const getAndRemoveLocalStorageState = (key: string) => {
+    const stateFromLocalStorage = getLocalStorageState(key);
+    removeLocalStorageState(key);
+    return stateFromLocalStorage;
+  };
+
+  const load = async () => {
+    const stateFromStorage = await chrome.storage.sync.get(key);
+
+    // As we have moved from localStorage to chrom.storage api,
+    // it's crucial not to lose the data, stored in localStorage so far.
+    // That's why we do the sync here - get the data from local storage
+    // and flush the localStorage[key].
+    const stateFromLocalStorage = getAndRemoveLocalStorageState(key);
+
+    const data = stateFromLocalStorage
+      ? stateFromLocalStorage
+      : stateFromStorage[key];
+
+    if (data) onLoadedFromStorage(data);
+
+    setIsLoaded(true);
+  };
+
+  const persist = async (key: string, state: T) => {
+    await chrome.storage.sync.set({ [key]: state });
+  };
+
+  useEffect(() => {
+    if (!isLoaded) load();
   }, [isLoaded]);
 
   useEffect(() => {
-    if (isLoaded) localStorage.setItem(key, JSON.stringify(state));
-  }, [state]);
+    if (isLoaded) persist(key, state);
+  }, [key, state]);
 }
